1c31c02a0701f0269e4d6caaabc32b540ffc203c
mostafa osama <hitmanx97@gmail.com>
2024-04-25 18:35:40 +0200

    first commit

---
+++ src/main/java/com/library/demo/LibraryManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.library.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		git SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
